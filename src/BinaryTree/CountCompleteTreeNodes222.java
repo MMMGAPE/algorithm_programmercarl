47033df21bb4226aafd9707c128dcc09fcd0ef25
jbmu <jbmu@ccxi.com.cn>
2022-05-12 15:28:02 +0800

    二叉树11 222.完全二叉树的节点个数

---
+++ src/BinaryTree/CountCompleteTreeNodes222.java
@@ -0,0 +1,20 @@
+package BinaryTree;
+
+public class CountCompleteTreeNodes222 {
+    public int countNodes(TreeNodeSample root) {
+        return getCount(root);
+    }
+    public int getCount(TreeNodeSample node){
+        if (node == null) return 0;
+        int leftCount = getCount(node.left);
+        int rightCount = getCount(node.right);
+        if (node.left==null&&node.right==null)
+            return 1;
+        else if (node.left!=null&&node.right==null)
+            return leftCount+1;
+        else if (node.left==null)
+            return rightCount+1;
+        else
+            return leftCount+rightCount+1;
+    }
+}
