5d9ca1f9b5306cb5cae06b8630ed62ba6b5e7bf6
jbmu <jbmu@ccxi.com.cn>
2022-05-16 21:05:17 +0800

    二叉树18 112. 路径总和

---
+++ src/BinaryTree/PathSum112.java
@@ -0,0 +1,27 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathSum112 {
+    public boolean hasPathSum(TreeNodeSample root, int targetSum) {
+        List<Integer> sumList = new ArrayList<>();
+        getSumList(root,sumList,0);
+        return sumList.contains(targetSum);
+    }
+    public void getSumList(TreeNodeSample node,List<Integer> sumList,int perSum){
+        if(node!=null){
+            perSum = perSum+node.val;
+            if (node.left!=null){
+                getSumList(node.left,sumList,perSum);
+            }
+            if (node.right!=null){
+                getSumList(node.right,sumList,perSum);
+            }
+            if (node.left==null&&node.right==null){
+                sumList.add(perSum);
+            }
+        }
+    }
+
+}
